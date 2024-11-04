26a8673fb90a9c1b82b9156410aeb371076107e1
Adrian Mungroo <adrianmungroo12@gmail.com>
2024-11-04 10:47:21 -0500

    Flipped images again

--- pages/7_load_profile_comparison.py
+++ pages/7_load_profile_comparison.py
@@ -18,7 +18,7 @@ with col1:
     st.markdown(
         """
         <div style="text-align: center;">
-            <img src="https://i.imgur.com/TnE4QQC.png" width="620">
+            <img src="https://i.imgur.com/sMmusZQ.png" width="620">
         </div>
         """,
         unsafe_allow_html=True
@@ -28,7 +28,7 @@ with col2:
     st.markdown(
         """
         <div style="text-align: center;">
-            <img src="https://i.imgur.com/sMmusZQ.png" width="620">
+            <img src="https://i.imgur.com/TnE4QQC.png" width="620"> 
         </div>
         """,
         unsafe_allow_html=True

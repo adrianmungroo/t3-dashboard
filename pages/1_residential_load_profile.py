99202481734f930c8e63090b4966e41c3ffe69d6
Adrian Mungroo <adrianmungroo12@gmail.com>
2024-11-07 02:11:12 -0500

    residential washer comment tab2

--- pages/1_residential_load_profile.py
+++ pages/1_residential_load_profile.py
@@ -101,6 +101,7 @@ with col2:
         """,
         unsafe_allow_html=True
     )
+    st.markdown("<div style='text-align: center;'><p>Note: This household used washer/dryer at 9pm</p></div>", unsafe_allow_html=True)
 
 st.divider()
 st.caption(APP_SUBTITLE)

0c0dd35b555ea1ccfe2d94d01cb96b59bb7ce346
Adrian Mungroo <adrianmungroo12@gmail.com>
2024-11-14 17:25:14 -0500

    Added energy burden data to yearly map

--- pages/4_pv_capability_by_landuse.py
+++ pages/4_pv_capability_by_landuse.py
@@ -3,10 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.graph_objs as go
 
-link_dict = {'Apartments (Class X)': ["https://i.imgur.com/tg5AmQh.gif", "https://i.imgur.com/a4Kd2Q2.png"],
-             'Schools' : ["https://i.imgur.com/UuVIZE3.gif", "https://i.imgur.com/ZGJvqFL.png"],
-             'Churches' : ["https://i.imgur.com/i9YAmH6.gif", "https://i.imgur.com/3fiFoB5.png"],
-             'Warehouses' : ["https://i.imgur.com/Ou1rFvB.gif", "https://i.imgur.com/qOb6zRG.png"],
+link_dict = {'Apartments (Class X)': ["https://i.imgur.com/tg5AmQh.gif", "https://i.imgur.com/k6UmhER.png"],
+             'Schools' : ["https://i.imgur.com/UuVIZE3.gif", "https://i.imgur.com/KjnreM5.png"],
+             'Churches' : ["https://i.imgur.com/i9YAmH6.gif", "https://i.imgur.com/htlTzNI.png"],
+             'Warehouses' : ["https://i.imgur.com/Ou1rFvB.gif", "https://i.imgur.com/NBayTpO.png"],
              }
 
 APP_TITLE = "PV Capability"

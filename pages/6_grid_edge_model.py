41d22abb149bc8b75351b1d9db4c97dd6beab125
Adrian Mungroo <adrianmungroo12@gmail.com>
2025-01-29 13:26:28 -0500

    Added combined counties to grid edge tab

--- pages/6_grid_edge_model.py
+++ pages/6_grid_edge_model.py
@@ -1,4 +1,26 @@
 import streamlit as st
 
-st.header("Hello World!")
-st.write("Grid edge updates will go here!")
+APP_TITLE = "Grid Edge Model"
+APP_SUBTITLE = "Dr. Jung-Ho Lewe, Dr. David Solano, Dr. Scott Duncan, Hyun Woo Kim, Adrian Mungroo, Meiwen Bi, Imran Aziz and Yunmei Guan"
+
+st.set_page_config(
+    page_title=APP_TITLE,
+    page_icon="⚡",
+    layout='wide'
+)
+
+st.header('Grid Edge Model Expansion')
+st.markdown('##### As seen in the gif below, we have expanded the reach of our grid energy usage models into Forsyth and DeKalb, with Gwinnett just around the corner!')
+
+st.divider()
+
+st.markdown(f"<div style='text-align: center;'><h5> Total Electrical Energy Usage (Fulton, Forsyth, DeKalb) </h5></div>", unsafe_allow_html=True)
+
+st.markdown(
+    f"""
+    <div style="text-align: center;">
+        <img src=https://i.imgur.com/zUrv2rx.gif width="500">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
